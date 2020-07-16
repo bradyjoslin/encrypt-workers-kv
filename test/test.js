db89b7385fbb2bb909378ee0883ec751c74965f7
Joslin, Brady W (Brady) <brady@bradyjoslin.com>
2020-07-15 21:13:58 -0600

    add tests

--- test/test.js
+++ test/test.js
@@ -8,7 +8,11 @@ function sleep(ms) {
 }
 
 async function it_encrypts(t, url, input) {
-  let res = await fetch(url, { method: 'PUT', body: input })
+  let res = await fetch(url, {
+    method: 'PUT',
+    body: input,
+    headers: { 'Content-Type': 'text/plain' },
+  })
   t.is(res.status, 200)
   t.is(await res.text(), 'Secret stored successfully')
 }
@@ -25,29 +29,86 @@ async function it_decrypts(t, url, input) {
 it_decrypts.title = (providedTitle = '', input) => `${providedTitle} ${input}`
 
 test.serial(
-  'encrypt 1',
+  'it encrypts text with urls',
   it_encrypts,
   url,
-  'This is a long message with urls https://bradyjoslin.com.',
+  'This is a message with urls https://bradyjoslin.com/blog/ms-teams-webhook-bot/?hello=world',
 )
 
 test.serial(
-  'decrypt 1',
+  'it decrypts text with urls',
   it_decrypts,
   url,
-  'This is a long message with urls https://bradyjoslin.com.',
+  'This is a message with urls https://bradyjoslin.com/blog/ms-teams-webhook-bot/?hello=world',
 )
 
 test.serial(
-  'encrypt 2',
+  'it encrypts text with emojis and special chars',
   it_encrypts,
   url,
-  'This is a long message with urls https://bradyjoslin.com...',
+  'Some text with emojis 🏂 and special characters å',
 )
 
 test.serial(
-  'decrypt 2',
+  'it decrypts text with emojis and special chars',
   it_decrypts,
   url,
-  'This is a long message with urls https://bradyjoslin.com...',
+  'Some text with emojis 🏂 and special characters å',
 )
+
+test.serial(
+  'it encrypts long text',
+  it_encrypts,
+  url,
+  `Sint velit eveniet. Rerum atque repellat voluptatem quia rerum. Numquam excepturi
+   beatae sint laudantium consequatur. Magni occaecati itaque sint et sit tempore. Nesciunt
+   amet quidem. Iusto deleniti cum autem ad quia aperiam.
+   A consectetur quos aliquam. In iste aliquid et aut similique suscipit. Consequatur qui
+   quaerat iste minus hic expedita. Consequuntur error magni et laboriosam. Aut aspernatur
+   voluptatem sit aliquam. Dolores voluptatum est.
+   Aut molestias et maxime. Fugit autem facilis quos vero. Eius quibusdam possimus est.
+   Ea quaerat et quisquam. Deleniti sunt quam. Adipisci consequatur id in occaecati.
+   Et sint et. Ut ducimus quod nemo ab voluptatum.`,
+)
+
+test.serial(
+  'it decrypts long text',
+  it_decrypts,
+  url,
+  `Sint velit eveniet. Rerum atque repellat voluptatem quia rerum. Numquam excepturi
+   beatae sint laudantium consequatur. Magni occaecati itaque sint et sit tempore. Nesciunt
+   amet quidem. Iusto deleniti cum autem ad quia aperiam.
+   A consectetur quos aliquam. In iste aliquid et aut similique suscipit. Consequatur qui
+   quaerat iste minus hic expedita. Consequuntur error magni et laboriosam. Aut aspernatur
+   voluptatem sit aliquam. Dolores voluptatum est.
+   Aut molestias et maxime. Fugit autem facilis quos vero. Eius quibusdam possimus est.
+   Ea quaerat et quisquam. Deleniti sunt quam. Adipisci consequatur id in occaecati.
+   Et sint et. Ut ducimus quod nemo ab voluptatum.`,
+)
+
+test.serial('it encrypts simple json', it_encrypts, url, '{"hello":"world"}')
+
+test.serial('it decrypts simple json', it_decrypts, url, '{"hello":"world"}')
+
+test.serial('it encrypts large json', async (t) => {
+  let input = await fetch(
+    'https://api.github.com/search/repositories?q=workers%20kv',
+  ).then((res) => res.text())
+  let res = await fetch(url, {
+    method: 'PUT',
+    body: input,
+    headers: { 'Content-Type': 'text/plain' },
+  })
+  t.is(res.status, 200)
+  t.is(await res.text(), 'Secret stored successfully')
+})
+
+test.serial('it decrypts large json', async (t) => {
+  let input = await fetch(
+    'https://api.github.com/search/repositories?q=workers%20kv',
+  ).then((res) => res.text())
+  sleep(1000)
+  let res = await fetch(url)
+  t.is(res.status, 200)
+  t.is(await res.text(), input)
+})
